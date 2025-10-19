b979bc15c6b95519cd782014501f6ee24e58eed9
Jonatan Rodríguez <jonodomongos@hotmail.com>
2025-10-19 22:52:13 +0200

    commit inicial

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+    /*
+    INSTRUCCIONES:
+        1. Rellenar ArrayList y mostrar resultado:
+            Crear un arraylist mediante un bucle con los números del 20 al 50.
+            Mostrar después todo el contenido del arraylist final.
+        2. Preguntar al usuario elementos típicos de la fiesta de Sant Jordi.
+            Para acabar, escribir 'salir'. Al final, mostrar todos los elementos introducidos por el usuario.
+        3. CRUD: recepción internacional... y universal!
+            Te han encargado organizar la recepción oficial para el primer contacto extraterrestre, en la montaña de Montserrat.
+            Solo unos pocos en el mundo pueden asistir. ¿A quién enviarías? De momento la lista sólo puede ser de cinco elegid@s.
+            Pero dos de ellos, en los puestos 2 y 3, se asustan y se dan de baja, y entra entonces el personaje X (darle nombre) en el puesto 2.
+            La persona que está en el último puesto, se pone enferma y te recomienda que pongas en su lugar al personaje Y (darle nombre). Ojo con saber qué índice es el último
+            En el último momento, se añade el personaje Z (darle nombre) al puesto vacante (puesto 3).
+            Ves mostrando a cada paso cómo va quedando la lista actualizada: los medios están muy interesados en seguir la noticia al minuto!
+     */
+    }
+}
